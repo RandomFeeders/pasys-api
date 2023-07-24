bb402d49fa9b4dd91f2eb9f602c58c9ca8ba0cf8
Utahh <kaw.alveslima@gmail.com>
2023-07-23 23:52:22 -0300

    feat: add first controller

---
+++ Pasys/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Pasys.Models;
+
+namespace Pasys.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<UserModel>> FetchAllUsers()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Pasys/Models/TaskModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Pasys.Models
+{
+    public class TaskModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; } //pode ser null
+        public string? Description { get; set; } //pode ser null
+        public int Status { get; set; }
+    }
+}
